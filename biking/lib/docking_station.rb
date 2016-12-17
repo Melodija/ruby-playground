254ae753478ddbea6399a9b2e1662a35b8e5022b
Ruta <ruta@baltiejus.com>
2016-12-17 12:40:54 +0000

    array of bikes added

--- biking/lib/docking_station.rb
+++ biking/lib/docking_station.rb
@@ -3,18 +3,27 @@ require_relative 'bike'
 class DockingStation
   attr_reader :bike
 
+  def initialize
+    @bikes = []
+  end
+
   def release_bike
     bike_available?
-    @bike
+    @bikes.pop
   end
 
   def dock bike
-    @bike = bike
+    dock_full?
+    @bikes << bike
   end
 
   private
 
   def bike_available?
-    fail 'There are no bikes in dock!' unless @bike
+    fail 'There are no bikes in dock!' if @bikes.empty?
+  end
+
+  def dock_full?
+    fail 'Docking station is full.' if @bikes.count >= 20
   end
 end
--- biking/spec/docking_station_spec.rb
+++ biking/spec/docking_station_spec.rb
@@ -24,16 +24,24 @@ describe DockingStation do
     end
   end
 
+  describe '#dock' do
     it 'docks a bike' do
       bike = Bike.new
-      expect(subject.dock(bike)).to eq bike
+      expect(subject.dock(bike)).to eq [bike]
     end
 
     it 'returns docked bike' do
       bike = Bike.new
       subject.dock(bike)
-      expect(subject.bike).to eq bike
+      expect(subject.bike).to eq nil
     end
+
+    it 'raises error if docking station already full' do
+      20.times { subject.dock(Bike.new) }
+      expect { subject.dock(Bike.new) }.to raise_error 'Docking station is full.'
+    end
+  end
+
 
 
 end
